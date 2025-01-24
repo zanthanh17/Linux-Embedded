1287939d0a99d4b4ec4c99156d96aa29878207b9
WTonion17 <vanthanh1752k3@gmail.com>
2025-01-24 15:17:23 +0700

    first commit

---
+++ Lesson1/assigment/linking/src/helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+void helloWorld()
+{
+    printf("hello World\n");
+}
---
+++ Lesson1/assigment/linking/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "hello.h"
+
+int main(void)
+{
+    helloThanh();
+    helloWorld();
+    return 0;
+}
